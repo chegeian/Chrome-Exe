73b2595fc585fcb87182692559c9513486e1524a
Ian chege <wizard-chege@pop-os.localdomain>
2024-01-31 16:09:03 +0300

    edit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
- 
- let inputBtn = document.getElementById("input-btn")
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-  inputBtn.addEventListener("click", function(){
-
-  })
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+})
+       
+     for (let i = 0 ; i > myLeads.length; i++){
+             ulEl.textcontent += myLeads[1] 
+     }
