e8168fd89e080ce2b209e880bc265c615a02ed32
Ian chege <wizard-chege@pop-os.localdomain>
2024-02-05 20:18:51 +0300

    delete

--- index.js
+++ index.js
@@ -2,27 +2,37 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads" ,Json.stringify(myLeads))
-    return renderLeads()
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
 })
 
-function renderLeads (){
-let listItems = ""
-for (let i = 0; i < myLeads.length; i++) {
-  listItems += `
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
         `
-    let leadsFromLocalStorage  =  localStorage.getItem("myLeads" Json.parse(myLeads))
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML = listItems
-}
-
-
